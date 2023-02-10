91900b3176b5d7818b45ed9b7eaba8326a3f8470
Krzysztof Rudnicki <krzysztofrudnicki0@gmail.com>
2023-02-10 19:35:17 +0100

    feat: further minimize app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RefresherCustomEvent } from '@ionic/angular';
 
-import { DataService, Message } from '../services/data.service';
 
 @Component({
   selector: 'app-home',
@@ -9,16 +8,12 @@ import { DataService, Message } from '../services/data.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private data: DataService) { }
+  constructor() { }
 
   refresh(ev: any) {
     setTimeout(() => {
       (ev as RefresherCustomEvent).detail.complete();
     }, 3000);
-  }
-
-  getMessages(): Message[] {
-    return this.data.getMessages();
   }
 
 }
